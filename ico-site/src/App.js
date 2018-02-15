fc3211787a0978b35e5b0227d84004eb64206316
Mikegg89 <Mikegg21@gmail.com>
2018-02-15 02:22:43 -0600

    got the header set up for the most part and the body frame setup

--- ico-site/src/App.js
+++ ico-site/src/App.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 import logo from './ICO-logo.png'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+    <div className="app">
       <div className="nav-bar">
-        <ul>
-          <li><img src={logo} alt="ico-logo" className="logo" /></li>
-          <li className="nav-list-item">search</li>
-        </ul>
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <img src={logo} alt="ico-logo" className="logo" />
+            </div>
+            <div className="col search-nav">
+              Search
+            </div>
+          </div>
+        </div>
       </div>
+      <div className="top-body">
+      </div>
+      <div className="middle-body">
+      </div>
+      <div className="bottom-body">
+      </div>
+    </div>
+
     );
   }
 }
